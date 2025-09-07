510f294615c4f8c24d7d236ffb02c36db4f1fddd
Aron Bazini <123810757+numbhill@users.noreply.github.com>
2025-09-07 05:14:34 +0200

    refactor: rely on HasUuids for UUID generation

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Str;
 
 class Payment extends Model
 {
@@ -13,17 +12,6 @@ class Payment extends Model
 
     public $incrementing = false;
     protected $keyType = 'string';
-
-    protected static function boot()
-    {
-        parent::boot();
-        static::creating(function ($model) {
-            // Only set a UUID on create (if not already set)
-            if (!$model->getKey()) {
-                $model->{$model->getKeyName()} = (string)Str::uuid();
-            }
-        });
-    }
 
     public function invoice(): BelongsTo
     {
